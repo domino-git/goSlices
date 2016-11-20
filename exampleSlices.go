ce98788e433641624f820d54230bb4458d8b2790
Dominik Stefanowicz <domino.stefano@gmail.com>
2016-11-20 16:55:23 +0100

    Print slice by slicing

--- exampleSlices.go
+++ exampleSlices.go
@@ -17,10 +17,22 @@ func AppendByte(slice []byte, data ...byte) []byte {
 	return slice
 }
 
+func PrintSlice(slice []byte) {
+	fmt.Printf("slice[:]   %d\n", slice[:])   // slice[:]  [1 2 3 4 5 6]
+	fmt.Printf("slice[0:]  %d\n", slice[0:])  // slice[0:] [1 2 3 4 5 6]
+	fmt.Printf("slice[:0]  %d\n", slice[:0])  // slice[:0] []
+	fmt.Printf("slice[0:0] %d\n", slice[0:0]) // slice[0:0] []
+	fmt.Printf("slice[1:]  %d\n", slice[1:])  // slice[1:] [2 3 4 5 6]
+	fmt.Printf("slice[:1]  %d\n", slice[:1])  // slice[:1] [1]
+	fmt.Printf("slice[:2]  %d\n", slice[:2])  // slice[:1] [1 2]
+	fmt.Printf("slice[3:5] %d\n", slice[3:5]) // slice[3:5] [4 5]
+}
+
 func main() {
 	fmt.Println("Below is presented some information about slices in Goland.")
 
 	var slice_1, slice_2, slice_3 []byte
+	slice_4 := []byte{1, 2, 3, 4, 5, 6}
 	slice_1 = make([]byte, 3, 5)
 	slice_1 = append(slice_1, 6, 6, 6) // append() automatically extend a slice
 	slice_2 = []byte{1, 2, 3, 4}
@@ -29,4 +41,5 @@ func main() {
 	slice_1 = AppendByte(slice_1, 9, 9, 9)
 	slice_3 = AppendByte(slice_1, slice_2...)
 	fmt.Println(slice_3)
+	PrintSlice(slice_4)
 }
